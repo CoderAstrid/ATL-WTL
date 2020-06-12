43fd74e5c766a4c062453b98c2c8c64f431cdb30
CoderAstrid <astrid.wang1229@gmail.com>
2020-06-11 20:27:35 -0700

    add the batch file for removing the unneeded files.

---
+++ WTL/HsvColorMap/DevClean_vc6_vc2008_vc2015.bat
@@ -0,0 +1,28 @@
+@echo --------------------------------------------
+@echo Delete and Remove the 'Debug', Release' Dir
+@echo      and NCB files for your Clean Data(_High Jewel_)
+@echo --------------------------------------------
+@echo  (Note: Confirm the current Directory...)
+@echo off
+pause
+@echo Starting...
+@echo Removing folders...
+FOR /R . %%d IN (.) DO rd /s /q "%%d\Debug" 2>nul
+FOR /R . %%d IN (.) DO rd /s /q "%%d\obj" 2>nul
+FOR /R . %%d IN (.) DO rd /s /q "%%d\x64" 2>nul
+FOR /R . %%d IN (.) DO rd /s /q "%%d\Release" 2>nul
+FOR /R . %%d IN (.) DO rd /s /q "%%d\Publish" 2>nul
+FOR /R . %%d IN (.) DO rd /s /q "%%d\ipch" 2>nul
+FOR /R . %%d IN (.) DO rd /s /q "%%d\_LeakDetectDebug" 2>nul
+FOR /R . %%d IN (.) DO rd /s /q "%%d\.vs" 2>nul
+@echo Removing files...
+FOR /R . %%d IN (.) DO del "%%d\*.ncb" 2>nul
+FOR /R . %%d IN (.) DO del "%%d\*.sdf" 2>nul
+FOR /R . %%d IN (.) DO del "%%d\*.pdb" 2>nul
+FOR /R . %%d IN (.) DO del "%%d\*.ilk" 2>nul
+FOR /R . %%d IN (.) DO del "%%d\*.exp" 2>nul
+FOR /R . %%d IN (.) DO del "%%d\*.ipch" 2>nul
+FOR /R . %%d IN (.) DO del "%%d\*.VC.db" 2>nul
+FOR /R . %%d IN (.) DO del "%%d\*.VC.opendb" 2>nul
+@echo Finished...Thanks!
+@echo. & pause
